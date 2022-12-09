2c4a5ddbecb8a18ab06504b2bbd255003717f50a
Paul Julian <36565183+SwampThingPaul@users.noreply.github.com>
2022-12-09 14:39:25 -0500

    edits

---
+++ slides/makefile.R
@@ -0,0 +1,6 @@
+
+## makefile.R
+### knits and moves files to folder for online sharing
+# Converts HTML slides to pdf
+Sys.setenv(PAGEDOWN_CHROME="C:/Users/Paul Julian/AppData/Local/Google/Chrome/Application/chrome.exe")
+# pagedown::chrome_print("./slides/20230117_WERP_Listening.Rmd")
